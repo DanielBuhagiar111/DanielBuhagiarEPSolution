4f1b8c1c5f23d45cc0fc0b8b02a4d284a0c50d90
Daniel <daniel.buhagiar.g53167@mcast.edu.mt>
2025-04-01 21:49:49 +0200

    Created the project

---
+++ Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SWD62ADanielBuhagiarEPHA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
